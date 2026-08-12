25d4d8245bff7d8258281d9ed13ad65f2ea529ce
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2026-08-11 19:40:23 -0700

    Release v0.6.0

--- launcher/release_metadata.py
+++ launcher/release_metadata.py
@@ -12,7 +12,7 @@ PRODUCT_NAME = APP_NAME
 # reject anything that is not a dotted number. A pre-release qualifier like
 # "-rc1" lives in VERSION_QUALIFIER instead and is shown to the user via
 # DISPLAY_VERSION, never handed to the build.
-PRODUCT_VERSION = "0.5.3"
+PRODUCT_VERSION = "0.6.0"
 FILE_VERSION = PRODUCT_VERSION
 # "" for a normal release; "-rc1" / "-beta1" while a version is under test.
 VERSION_QUALIFIER = ""
